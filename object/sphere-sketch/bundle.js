b84f4b2e29cd4aad3834a177e8748553e9ee976b
tyw66 <2639144635@qq.com>
2018-03-20 17:45:41 +0800

    没啥修改

--- object/sphere-sketch/bundle.js
+++ object/sphere-sketch/bundle.js
@@ -8,6 +8,7 @@ var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
 function pageStart(){
+	//建立摄像机
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight,1, 2000 );
 	camera.position.z = 30;
 	camera.position.x = 0;
@@ -18,121 +19,79 @@ function pageStart(){
 		z : 0
 	});
 	
-	
-	
-	scene = new THREE.Scene();
-	
+	//建立场景	
+	scene = new THREE.Scene();	
 	var ambient = new THREE.AmbientLight(0x101030);
 	scene.add( ambient );
-
+	
+	//建立灯光
 	var directionalLight = new THREE.DirectionalLight( 0xffeedd );
 	directionalLight.position.set(0, 0, 1 ).normalize();
-	scene.add( directionalLight );
+	                
+	var light = new THREE.DirectionalLight(0xFF0000, 1.0, 0);
+    light.position.set(100, 100, 200);
+	
+	scene.add( light );
 	
 	
-	// texture
-
+	
+	//导入贴图
 	var manager = new THREE.LoadingManager();
 	manager.onProgress = function ( item, loaded, total ) {
-
 		console.log( item, loaded, total );
-
 	};
-
 	var textureLoader = new THREE.TextureLoader( manager );
-	var texture = textureLoader.load( "./textures/sphere_baked.png" );
+	var texture = textureLoader.load( "textures/test/male.jpg" );
 
-	// model
-
+	//导入模型
 	var onProgress = function ( xhr ) {
 		if ( xhr.lengthComputable ) {
 			var percentComplete = xhr.loaded / xhr.total * 100;
 			console.log( Math.round(percentComplete, 2) + '% downloaded' );
 		}
 	};
-
+	
 	var onError = function ( xhr ) {
 	};
 
 	var loader = new THREE.OBJLoader( manager );
-	loader.load( "./models/sphere1.obj"), function ( object ) {
-
+	loader.load('models/test/male.obj', function ( object ) {
 		object.traverse( function ( child ) {
-
 			if ( child instanceof THREE.Mesh ) {
-
 				child.material.map = texture;
-
 			}
-
 		} );
 
-		object.position.y = - 95;
+		object.position.x = 0;
+		object.position.y = 0;
+		object.position.z = 0;
+		
 		scene.add( object );
 
-	}, 
-	onProgress, onError );
+	}, onProgress, onError );
 	
 	
-		renderer = new THREE.WebGLRenderer();
-		renderer.setPixelRatio( window.devicePixelRatio );
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		
-		
-		var container = document.getElementById('container');
-		
-		container.appendChild( renderer.domElement );
-
-		document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
-		//
-
-	//	window.addEventListener( 'resize', onWindowResize, false );
-
-
-
-
-
-
-}
-
-
-
-function onWindowResize() {
-
-	windowHalfX = window.innerWidth / 2;
-	windowHalfY = window.innerHeight / 2;
-
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-
+	
+	//建立渲染器
+	renderer = new THREE.WebGLRenderer();
+	//renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setClearColor(0xCCCCCC, 1.0);
+
+	
+	var container = document.getElementById('container');	
+	container.appendChild( renderer.domElement );
+	
+
+	renderer.render( scene, camera );
+
 
 }
-
-function onDocumentMouseMove( event ) {
-
-	mouseX = ( event.clientX - windowHalfX ) / 2;
-	mouseY = ( event.clientY - windowHalfY ) / 2;
-
-}
-
-//
 
 function animate() {
 
 	requestAnimationFrame( animate );
-	render();
-
-}
-
-function render() {
-
-	camera.position.x += ( mouseX - camera.position.x ) * .05;
-	camera.position.y += ( - mouseY - camera.position.y ) * .05;
-
-	camera.lookAt( scene.position );
-
 	renderer.render( scene, camera );
 
 }
+
