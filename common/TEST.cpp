dcca734c94565d02f18db0d3e6b931fd8993dc43
tyw66 <2639144635@qq.com>
2018-04-02 15:53:00 +0800

    修改common文件夹

--- common/TEST.cpp
+++ common/TEST.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "basic_type.hpp"   
+#include "color.hpp"   
+#include "vec.hpp"   
 
 //½á¹¹Ìå - ²âÊÔ 
 int main(){
@@ -14,12 +15,20 @@ int main(){
 	Color cyan =   (red - white)*0.5;
 	printf("cyan-> r:%d,g:%d,b:%d\n\n",cyan.r,cyan.g,cyan.b);
 	
+	Color gray(100,100,100);
+	Color gray_light = gray*2;
+	printf("gray_light:%d,%d,%d\n",gray_light.r,gray_light.g,gray_light.b);
 	
+	Color gray_light2 (gray_light);
+	printf("gray_light2-> r:%d,g:%d,b:%d\n\n",gray_light2.r,gray_light2.g,gray_light2.b);
+	
+	/*
 	Vec v1(4,1,1);
 	Vec v2(3,2,3);
 	Vec v= v1-v2;
  	printf("x:%f,y:%f,z:%f\n",v.x,v.y,v.z);
 
 	printf("dot:%f",dot(v1,v2));
+	*/
 	
 }
--- common/vec.hpp
+++ common/vec.hpp
@@ -1,21 +1,4 @@
 #include "math.h"
-struct Color3{
-	int r, g, b;
-	//构造函数 
-	Color3(unsigned int r_=0,unsigned int g_=0 ,unsigned int b_=0) { 
-		r=r_>255?255:r_; g=g_>255?255:g_;b=b_>255?255:b_;
-	}
-	//拷贝函数todo
-		
-	//颜色加法 
-	Color3 operator+(const Color3& c)const{return Color3(r+c.r, g+c.g, b+c.b);}
-	//颜色减法 
-	Color3 operator-(const Color3& c)const{return Color3(r-c.r, g-c.g, b-c.b);}
-	//颜色数乘 
-	Color3 operator*(const double factor)const{return Color3(factor*r, factor*g, factor*b);}
-
-};
-
 struct Vec3{
 	double x, y, z;
 	//构造函数
@@ -28,6 +11,8 @@ struct Vec3{
 	Vec3 operator-(const Vec3& v)const{return Vec3(x-v.x, y-v.y, z-v.z);}
 	//向量的模 
 	double norm()const{return sqrtf(x*x+y*y+z*z);}
+	//向量数乘
+	Vec3 operator*(const double number){return Vec3(x*number, y*number, z*number);}
 };
 
 //向量点乘
@@ -38,7 +23,5 @@ double dot(const Vec3& lhs, const Vec3& rhs){
 //向量叉乘	
 
 
-
-typedef struct Color3 Color;
 typedef struct Vec3 Pos;
 typedef struct Vec3 Vec;
